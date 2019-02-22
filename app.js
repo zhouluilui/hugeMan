e87e7a9356ff399820abb714decbdd1a4d3ce729
zhouluilui <18181813593@163.com>
2019-02-22 16:42:33 +0800

    update banner

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-const express = require
+const express = require("express");//引入模块
+const bodyParser = require("body-parser");
+const pool = require("./pool");
+var app = express();//返回一个函数，创建对象
+app.listen(3001);//
+app.use(bodyParser.urlencoded({extended:false}))//使用bodyPaser中间件
+app.use(express.static('public'))//托管静态资源到public
